ce11cdfb0c9370a902e1b70ee2df2f5e1aa8fb0f
Adri Rajaraman <arajaraman@wpi.edu>
2023-05-19 13:28:07 -0400

    added scripts and data for torison model check tests

--- data_collection_scripts/optitrack_init.m
+++ data_collection_scripts/optitrack_init.m
@@ -7,7 +7,7 @@
 % Note : The NatNetML.DLL assembly depends on NatNet.dll, so make sure they
 % are both in the same folder and/or path if you move them.
 % dllPath = fullfile('.\NatNet_SDK_4.0\NatNetSDK\lib\x64\NatNetML.dll');
-dllPath = fullfile('D:\FichStuff\CTR_Spring23\CTR_ControlBoard_Firmware\NatNet_SDK_4.0_edited\NatNet_SDK_4.0\NatNetSDK\lib\x64\NatNetML.dll');
+dllPath = fullfile('D:\FichStuff\CTR_Summer23\CTR_Kinematic_Verification\data_collection_scripts\NatNet_SDK_4.0\NatNetSDK\lib\x64\NatNetML.dll');
 % dllPath = fullfile('C:\Users\ninab\OneDrive\Documents\WPI\2022-2023\Spring Semester\RBE 598 Directed Research\CTR_ControlBoard_Firmware\NatNet_SDK_4.0_edited\NatNet_SDK_4.0\NatNetSDK\lib\x64\NatNetML.dll');
 assemblyInfo = NET.addAssembly(dllPath);
 theClient = NatNetML.NatNetClientML(0); % Input = iConnectionType: 0 = Multicast, 1 = Unicast
