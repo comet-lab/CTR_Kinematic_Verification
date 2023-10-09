9916bfa4d7447dbaead3fc418a9170899f2176e7
chrisdemaio <62364581+chrisdemaio@users.noreply.github.com>
2023-10-09 15:18:34 -0400

    comments/refactor and also sim of obvious curvature results (#11)

--- data_collection_scripts/Tube.m
+++ data_collection_scripts/Tube.m
@@ -1,16 +1,16 @@
 classdef Tube < handle
 
     properties
-        id = 0
-        od = 0
-        r = 0
-        k = 0
-        l = 0
-        d = 0
-        E = 0
-        I = 0
-        J = 0
-        G = 0
+        id = 0          % inner diameter            [m]
+        od = 0          % outer diameter            [m]
+        r = 0           % Bending radius            [m]
+        k = 0           % curvature                 [m^-1]
+        l = 0           % uncut length              [m]
+        d = 0           % arc length                [m]
+        E = 0           % Young's modulus           [N/m^2]
+        I = 0           % 2nd moment of inertia     [m^4]
+        J = 0           % polar moment of inertia   [m^4]
+        G = 0           % shear modulus             [N/m^2]
 
     end
 
@@ -23,7 +23,7 @@ classdef Tube < handle
             self.l = l;
             self.d = d;
             self.E = E;
-            self.I = (pi/4)*(od^4 - id^4);
+            self.I = (pi/64)*(od^4 - id^4);
             self.J = 2*self.I;
             self.G = self.E/(2 * (1 + 0.217));
 %             self.G = 3.93*10^9;
